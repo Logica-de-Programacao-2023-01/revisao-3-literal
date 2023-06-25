95175eaf823786764d608583668e11d303be1bce
Felipe Rodrigues <95952269+felipethecreator@users.noreply.github.com>
2023-06-25 15:47:41 -0300

    Update q3.go

--- q3/q3.go
+++ q3/q3.go
@@ -1,9 +1,41 @@
 package q3
 
-//Você está subindo uma escada. Leva n passos para chegar ao topo.
-//
-//A cada vez, você pode subir 1 ou 2 degraus. De quantas maneiras distintas você pode subir até o topo?
+import "fmt"
 
-func ClimbStairs(n int) int {
-	return 0
+func FindMinMaxAverage(numbers []int) (int, int, float64, error) {
+	if len(numbers) == 0 {
+		return 0, 0, 0, fmt.Errorf("Lista vazia")
+	}
+	maior := -999999999999
+	menor := 9999999999999
+	var soma int
+	var media float64
+	if len(numbers) == 1 {
+		maior = numbers[0]
+		menor = numbers[0]
+		media = float64(numbers[0])
+		return maior, menor, media, nil
+	}
+	for i := 0; i < len(numbers); i++ {
+		if maior < numbers[i] {
+			maior = numbers[i]
+		}
+		if menor > numbers[i] {
+			menor = numbers[i]
+		}
+	}
+	j := 0
+	for i := 0; i < len(numbers); i++ {
+		if maior == numbers[i] {
+			continue
+		}
+		if menor == numbers[i] {
+			continue
+		}
+		soma += numbers[i]
+		numbers[j] = numbers[i]
+		j++
+	}
+	media = float64(soma) / float64(len(numbers)-2)
+	return maior, menor, media, nil
 }
