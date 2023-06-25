ccba475a3d54ad81b7724499a8f976f85e67410d
Felipe Rodrigues <95952269+felipethecreator@users.noreply.github.com>
2023-06-25 15:51:03 -0300

    Update q5.go

--- q5/q5.go
+++ q5/q5.go
@@ -1,39 +1,29 @@
 package q5
 
-import "fmt"
+import "strings"
 
-func ConvertTemperature(temp float64, fromScale string, toScale string) (float64, error) {
-	var finalTemp float64
-	switch fromScale {
-	case "C":
-		switch toScale {
-		case "F":
-			finalTemp = temp*9/5 + 32
-		case "K":
-			finalTemp = temp + 273.15
-		default:
-			return 0, fmt.Errorf("escala inválida")
-		}
-	case "F":
-		switch toScale {
-		case "C":
-			finalTemp = (temp - 32) * 5 / 9
-		case "K":
-			finalTemp = (temp-32)*5/9 + 273.15
-		default:
-			return 0, fmt.Errorf("escala inválida")
-		}
-	case "K":
-		switch toScale {
-		case "C":
-			finalTemp = temp - 273.15
-		case "F":
-			finalTemp = (temp-273.15)*9/5 + 32
-		default:
-			return 0, fmt.Errorf("escala inválida")
-		}
-	default:
-		return 0, fmt.Errorf("escala inválida")
+func reverse(str string) string {
+	runes := []rune(str)
+
+	for i, j := 0, len(runes)-1; i < len(runes)/2; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
 	}
-	return finalTemp, nil
+
+	return string(runes)
+}
+
+func IsPalindrome(s string) bool {
+	s = strings.ToLower(s)
+
+	specialChars := []string{":", ",", " "}
+
+	for _, char := range specialChars {
+		s = strings.ReplaceAll(s, char, "")
+	}
+
+	if reverse(s) == s {
+		return true
+	}
+
+	return false
 }
