257cedde4d17ace7cc22b8e8c1c95c3c58927f3a
Felipe Rodrigues <95952269+felipethecreator@users.noreply.github.com>
2023-06-25 15:52:05 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -1,26 +1,16 @@
 package q1
 
-import "fmt"
+func TwoSum(nums []int, target int) []int {
+	numMap := make(map[int]int)
 
-func CalculateDiscount(currentPurchase float64, purchaseHistory []float64) (float64, error) {
-	var media float64
-	var soma float64
-	for i := 0; i < len(purchaseHistory); i++ {
-		soma += purchaseHistory[i]
+	for j, num := range nums {
+		complement := target - num
+		if i, ok := numMap[complement]; ok {
+			return []int{i, j}
+		}
+
+		numMap[num] = j
 	}
-	media = soma / float64(len(purchaseHistory))
-	if currentPurchase <= 0 {
-		return 0, fmt.Errorf("valor da compra inválido")
-	} else if media > 1000 {
-		return currentPurchase * 0.2, nil
-	} else if soma > 1000 {
-		return currentPurchase * 0.1, nil
-	} else if len(purchaseHistory) == 0 {
-		return currentPurchase * 0.1, nil
-	} else if soma <= 1000 && soma > 500 {
-		return currentPurchase * 0.05, nil
-	} else if soma <= 500 {
-		return currentPurchase * 0.02, nil
-	}
-	return 0, nil
+
+	return nil
 }
