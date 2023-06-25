6be7ae7acfaf1ec3844005e4542c113ad099ac4d
Felipe Rodrigues <95952269+felipethecreator@users.noreply.github.com>
2023-06-25 15:51:51 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -1,23 +1,17 @@
 package q2
 
-import (
-	"fmt"
-	"strings"
-)
+import "strings"
 
-func AverageLettersPerWord(text string) (float64, error) {
-	speciais:= []string{"!","?","@","#","$",".",",","1","2","3","4","5","6","7","8","9"}
-	for _, caracter:= range speciais {
-		text = strings.ReplaceAll(text,caracter,"")
+func LongestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
 	}
-	palavras := strings.Fields(text)
-	if len(palavras) == 0 {
-		return 0, fmt.Errorf("texto vazio")
-		
+	prefix := strs[0]
+
+	for i := 1; i < len(strs) && len(prefix) > 0; i++ {
+		for !strings.HasPrefix(strs[i], prefix) {
+			prefix = prefix[:len(prefix)-1]
+		}
 	}
-	var totalpalavra int
-	for _, palavra:= range palavras {
-		totalpalavra += len(palavra)
-	}
-	return float64(totalpalavra) / float64(len(palavras)),nil
+	return prefix
 }
