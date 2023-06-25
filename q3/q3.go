be51f997af3ccb9f98283626b130278b6daadea4
Felipe Rodrigues <95952269+felipethecreator@users.noreply.github.com>
2023-06-25 15:51:37 -0300

    Update q3.go

--- q3/q3.go
+++ q3/q3.go
@@ -1,41 +1,17 @@
 package q3
 
-import "fmt"
+func ClimbStairs(n int) int {
+	if n == 1 {
+		return 1
+	}
 
-func FindMinMaxAverage(numbers []int) (int, int, float64, error) {
-	if len(numbers) == 0 {
-		return 0, 0, 0, fmt.Errorf("Lista vazia")
+	waysClimb := make([]int, n+1)
+	waysClimb[1] = 1
+	waysClimb[2] = 2
+
+	for i := 3; i <= n; i++ {
+		waysClimb[i] = waysClimb[i-1] + waysClimb[i-2]
 	}
-	maior := -999999999999
-	menor := 9999999999999
-	var soma int
-	var media float64
-	if len(numbers) == 1 {
-		maior = numbers[0]
-		menor = numbers[0]
-		media = float64(numbers[0])
-		return maior, menor, media, nil
-	}
-	for i := 0; i < len(numbers); i++ {
-		if maior < numbers[i] {
-			maior = numbers[i]
-		}
-		if menor > numbers[i] {
-			menor = numbers[i]
-		}
-	}
-	j := 0
-	for i := 0; i < len(numbers); i++ {
-		if maior == numbers[i] {
-			continue
-		}
-		if menor == numbers[i] {
-			continue
-		}
-		soma += numbers[i]
-		numbers[j] = numbers[i]
-		j++
-	}
-	media = float64(soma) / float64(len(numbers)-2)
-	return maior, menor, media, nil
+
+	return waysClimb[n]
 }
