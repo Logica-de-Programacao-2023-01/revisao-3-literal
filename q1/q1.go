1a5a506d427b3218b477f8a76a0e73717fa68d41
Felipe Rodrigues <95952269+felipethecreator@users.noreply.github.com>
2023-06-25 15:46:50 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -1,12 +1,26 @@
 package q1
 
-//Dado um array de números inteiros "nums" e um número inteiro "target", retorne os índices dos dois números cuja soma
-//seja igual a "target".
-//
-//Você pode assumir que cada entrada terá exatamente uma solução e não poderá usar o mesmo elemento duas vezes.
-//
-//Você pode retornar a resposta em qualquer ordem.
+import "fmt"
 
-func TwoSum(nums []int, target int) []int {
-	return nil
+func CalculateDiscount(currentPurchase float64, purchaseHistory []float64) (float64, error) {
+	var media float64
+	var soma float64
+	for i := 0; i < len(purchaseHistory); i++ {
+		soma += purchaseHistory[i]
+	}
+	media = soma / float64(len(purchaseHistory))
+	if currentPurchase <= 0 {
+		return 0, fmt.Errorf("valor da compra inválido")
+	} else if media > 1000 {
+		return currentPurchase * 0.2, nil
+	} else if soma > 1000 {
+		return currentPurchase * 0.1, nil
+	} else if len(purchaseHistory) == 0 {
+		return currentPurchase * 0.1, nil
+	} else if soma <= 1000 && soma > 500 {
+		return currentPurchase * 0.05, nil
+	} else if soma <= 500 {
+		return currentPurchase * 0.02, nil
+	}
+	return 0, nil
 }
