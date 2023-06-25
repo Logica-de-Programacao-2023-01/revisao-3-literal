e983aea51bfc68e461691726ebb572f2f2eb2519
Felipe Rodrigues <95952269+felipethecreator@users.noreply.github.com>
2023-06-25 15:47:13 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -1,9 +1,23 @@
 package q2
 
-//Escreva uma função para encontrar o prefixo comum mais longo entre um array de strings.
-//
-//Se não houver um prefixo comum, retorne uma string vazia "".
+import (
+	"fmt"
+	"strings"
+)
 
-func LongestCommonPrefix(strs []string) string {
-	return ""
+func AverageLettersPerWord(text string) (float64, error) {
+	speciais:= []string{"!","?","@","#","$",".",",","1","2","3","4","5","6","7","8","9"}
+	for _, caracter:= range speciais {
+		text = strings.ReplaceAll(text,caracter,"")
+	}
+	palavras := strings.Fields(text)
+	if len(palavras) == 0 {
+		return 0, fmt.Errorf("texto vazio")
+		
+	}
+	var totalpalavra int
+	for _, palavra:= range palavras {
+		totalpalavra += len(palavra)
+	}
+	return float64(totalpalavra) / float64(len(palavras)),nil
 }
