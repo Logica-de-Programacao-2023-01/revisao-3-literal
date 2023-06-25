8947df62e1856d22b32a762bedb8525322dabeec
Felipe Rodrigues <95952269+felipethecreator@users.noreply.github.com>
2023-06-25 15:51:21 -0300

    Update q4.go

--- q4/q4.go
+++ q4/q4.go
@@ -1,41 +1,16 @@
 package q4
 
-import "fmt"
+func SingleNumber(nums []int) int {
+	numMap := make(map[int]int)
 
-func CalculateFinalPrice(basePrice float64, state string, taxCode int) (float64, error) {
-	var precoFinal float64
-	var frete float64
-	var imposto float64
-
-	if basePrice <= 0 {
-		return 0, fmt.Errorf("preço base inválido")
+	for _, num := range nums {
+		numMap[num]++
 	}
 
-	switch state {
-	case "SP":
-		frete = 0.10
-	case "RJ":
-		frete = 0.15
-	case "MG":
-		frete = 0.20
-	case "ES":
-		frete = 0.25
-	default:
-		frete = 0.30
+	for i, num := range nums {
+		if numMap[num] == 1 {
+			return i
+		}
 	}
-
-	switch taxCode {
-	case 1:
-		imposto = 0.05
-	case 2:
-		imposto = 0.10
-	case 3:
-		imposto = 0.15
-	default:
-		return 0, fmt.Errorf("imposto não encontrado")
-	}
-
-	precoFinal = basePrice + basePrice*frete + basePrice*imposto
-
-	return precoFinal, nil
+	return -1
 }
